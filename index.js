c688f883f1835236f2fdde8cd6c0154e69c8f519
Gabe Lalasava <gabe@fijiwebdesign.com>
2018-01-26 08:02:09 +0800

    Gracefully close server when node exits due to SIGTERM, Exceptions or kill pid

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ var server = new Server({
 ]);
 
 server.start();
+
+function onExit(event) {
+  console.info('Closing server due to ' + event);
+  server.stop(function () {
+    console.info("Closed UpnpServer due to " + event);
+    process.exit();
+  });
+}
+
+//do something when app is closing
+process.on('exit', onExit.bind(null, 'exit'));
+
+//catches ctrl+c event
+process.on('SIGINT', onExit.bind(null, 'SIGINT'));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', onExit.bind(null, 'SIGUSR1'));
+process.on('SIGUSR2', onExit.bind(null, 'SIGUSR2'));
+
+//catches uncaught exceptions
+process.on('uncaughtException', onExit.bind(null, 'uncaughtException'));
