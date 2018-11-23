174cc08f539c7065cce8f655e46e06f043bcf76d
Gabe <gabe@fijiwebdesign.com>
2018-11-23 15:59:26 +0800

    Allow mounting custom directory

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var Server = require("upnpserver");
+const Server = require("upnpserver");
 
-var server = new Server({ 
+const path = process.argv[2];
+if (!path) {
+  throw new Error('A path to mount is required as last argument. eg: bitcast-server /Users/gabe/Movies')
+}
+
+const server = new Server({ 
     hostname: 'localhost',
     httpPort: 8888,
-    name: 'StreamCaster ' + (new Date().getTime()),
-    modelName: 'StreamCaster-1.0',
+    name: 'BitCast mount:' + path,
+    modelName: 'BitCast-1.0',
     log: true,
     logLevel: 'TRACE',
     ssdpLog: true,
     ssdpLogLevel: 'TRACE'
  }, [
-  { path: '/Users/chandra/Downloads', mountPoint: '/All', type: 'directory'},
-  { path: '/Users/chandra/Downloads', mountPoint: '/Movies', type: 'movie'},
-  { path: '/Users/chandra/Downloads', mountPoint: '/Music', type: 'music'},
+  { path, mountPoint: '/Media', type: 'directory'},
+  //{ path: '/Users/chandra/Downloads', mountPoint: '/Movies', type: 'movie'},
+  //{ path: '/Users/chandra/Downloads', mountPoint: '/Music', type: 'music'},
   //{ path: '/Users/chandra/Music/iTunes/iTunes Media', mountPoint: '/iTunes'}
 ]);
 
